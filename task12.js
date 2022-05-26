565f7664d86aef8d812d9b37656ba06db92e9942
pavan0409 <pavanchitguppa@gmail.com>
2022-05-27 00:06:49 +0530

    edit del added

--- task12.js
+++ task12.js
@@ -4,9 +4,6 @@ function saveToLocalStorage(event) {
     const name = event.target.username.value;
     const email = event.target.emailId.value;
     const phonenumber = event.target.phonenumber.value;
-    // localStorage.setItem('name', name);
-    // localStorage.setItem('email', email);
-    // localStorage.setItem('phonenumber', phonenumber)
     const obj = {
         name,
         email,
